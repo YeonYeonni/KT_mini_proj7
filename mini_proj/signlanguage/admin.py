cfc85ed1a9150ee834bb6736adf3d0e68f2dd15f
zelo0 <minorSunbi@gmail.com>
2022-11-22 15:43:55 +0900

    feat: 모델 관련 페이지 출력

--- mini_proj/signlanguage/admin.py
+++ mini_proj/signlanguage/admin.py
@@ -1,4 +1,8 @@
+import json
 from django.contrib import admin
+from django.db.models import Count, F, FloatField, IntegerField
+from django.db.models.functions import TruncDay, Cast
+from django.core.serializers.json import DjangoJSONEncoder
 
 # Register your models here.
 from .models import Result, AiModel
@@ -8,6 +12,26 @@ admin.site.register(Result)
 # admin.site.register(AiModel)
 
 @admin.register(AiModel)
-class EmailSubscriberAdmin(admin.ModelAdmin):
+class AiModelAdmin(admin.ModelAdmin):
     list_display = ("id", "name", "is_using", "file", "created") # display these table columns in the list view
     ordering = ("-created",)   
+
+    def changelist_view(self, request, extra_context=None):
+        # Aggregate new subscribers per day
+        chart_data = (
+            AiModel.objects
+            .values("name", "prediction_count", "answer_count")
+            .annotate(x = F("name"))
+            .annotate(y = Cast(Cast(F("answer_count"), FloatField()) / F("prediction_count") * 100, IntegerField() ) )
+            .values("x", "y")
+            .order_by("-name")
+        )
+
+        print(list(chart_data))
+
+        # Serialize and attach the chart data to the template context
+        as_json = json.dumps(list(chart_data), cls=DjangoJSONEncoder)
+        extra_context = extra_context or {"chart_data": as_json}
+
+        # Call the superclass changelist_view to render the page
+        return super().changelist_view(request, extra_context=extra_context)
