60789aeedce205baef86483f1bafdceda550abb0
zelo0 <minorSunbi@gmail.com>
2022-11-22 12:23:47 +0900

    feat: admin에서 리스트 보여주기

--- mini_proj/signlanguage/admin.py
+++ mini_proj/signlanguage/admin.py
@@ -5,4 +5,9 @@ from .models import Result, AiModel
 
 # 관리에서 Result 객체에 대해  기본 CRUD 관리를 한다. 
 admin.site.register(Result)
-admin.site.register(AiModel)
+# admin.site.register(AiModel)
+
+@admin.register(AiModel)
+class EmailSubscriberAdmin(admin.ModelAdmin):
+    list_display = ("id", "name", "is_using", "file", "created") # display these table columns in the list view
+    ordering = ("-created",)   
